ae794dec7e6162d18716ebb0a2c44d23a393856a
Andy Smart <asmart@iweb.co.uk>
2016-03-18 13:35:42 +0000

    Changed the custom layout handle example to add the customer group as a layout handle instead (better)

--- Plugin/Handles.php
+++ Plugin/Handles.php
@@ -2,13 +2,33 @@
 
 namespace Smartie\SampleModule\Plugin;
 
+use Magento\Customer\Model\Session;
 use Magento\Framework\View\Result\Page;
+use Magento\Store\Model\GroupFactory;
 
 class Handles
 {
+    protected $customerSession;
+
+    protected $groupFactory;
+
+    public function __construct(
+        Session $customerSession,
+        GroupFactory $groupFactory
+    ) {
+        $this->customerSession = $customerSession;
+        $this->groupFactory = $groupFactory;
+    }
+
     public function afterAddPageLayoutHandles(Page $subject)
     {
-        $subject->addHandle('custom_layout_handle');
+        $group = $this->groupFactory->create()
+            ->load($this->customerSession->getCustomerGroupId());
+        if ($group->getId() >= 0) {
+            $handle = sprintf("customer_group_%s", strtolower(preg_replace("/[^A-Za-z0-9]/", '_', $group->getName())));
+            $subject->addHandle($handle);
+        }
+
         return $subject;
     }
 }
